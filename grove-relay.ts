0a7765cd09ca5eb571a2747419e3c71faa63a9ce
MarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-04-07 08:22:45 +0200

    Create grove-relay.ts

---
+++ grove-relay.ts
@@ -0,0 +1,27 @@
+/**
+ * Functions for the Seeedstudio Grove Relay.
+ *
+ * @author MagicMarvMan
+ */
+
+
+//% weight=2 color=#1174EE block="Grove Relay"
+//% parts="grove_relay"
+namespace grove_mpr121 {
+
+    //% weight=210
+    //% blockId=grove_relay_on block="Relay on"
+    //% blockExternalInputs=1
+    //% parts="grove_relay"
+    export function relay_on(): void {
+      pins.digitalWritePin(DigitalPin.C16, 1)
+    }
+    
+    //% weight=210
+    //% blockId=grove_relay_off block="Relay off"
+    //% blockExternalInputs=1
+    //% parts="grove_relay"
+    export function relay_off(): void {
+      pins.digitalWritePin(DigitalPin.C16, 0)
+    }
+}
